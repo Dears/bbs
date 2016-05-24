77b0e977a9e2f9b9de701f83c8f2361cd35d56e7
Shiori Ogawa <s.ogawa211@gmail.com>
2016-05-25 00:59:35 +0900

    login.ect表示まで

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
-var http = require('http');
+'use strict'
 
-var server = http.createServer(function(req, res){
-  res.write('Hello.');
-  res.end();
-});
+const http = require('http');
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+const post = require('./routes/post');
+const ect = require('ect');
+const ectRenderer = ect({ watch: true, root: __dirname + '/views', ext : '.ect' })
+const bodyParser = require('body-parser');
 
-server.listen(3000);
-console.log('server listening');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ect');
+app.engine('ect', ectRenderer.render);
+
+// logを出力
+app.use(logger('dev'));
+
+
+app.get('/', post.login);
+
+app.listen(3000);
+console.log('server listening...');
