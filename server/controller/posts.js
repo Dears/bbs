f5d079cc8b60526442b81f58c69886977cbb0618
Shiori Ogawa <s.ogawa211@gmail.com>
2016-05-28 22:23:45 +0900

    MongoDBから記事データを取得表示

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -2,30 +2,22 @@
 
 const postAdapter = require('../service/adapter/post');
 
-//仮に表示するデータを配列で
-const posts = [
-  {id: '0', username:'ogawa', time:'ddd', text:'body0', },
-  {id: '1', username:'ogawa1', time:'dddd', text:'body1', },
-  {id: '2', username:'ogawa2', time:'ddddd', text:'body2', },
-  {id: '3', username:'ogawa3', time:'dddddd', text:'body3', },
-];
-
-exports.get = function(req, res) {
-  res.render('posts', { posts: posts });
+exports.get = function(req, res, next) {
+  // mongoDBからデータを渡す
+  postAdapter.getPostsAll(function(err, posts) {
+    if (err)
+      return next(err);
+    res.render('posts', { posts: posts });
+  });
 }
 
-exports.post = function(req, res) {
+exports.post = function(req, res, next) {
   let text = req.body.text;
 
-  posts.push({
-    id: posts.length,
-    username: 'ogawa' + posts.length,
-    time: 'ssss',
-    text: text
-  });
-
 // お試しでmongoにも入れてみる
-  postAdapter.insert('ogawa', text);
-
-  res.redirect('/posts');
+  postAdapter.insert('ogawa', text, function(err) {
+    if(err)
+      return next(err);
+    res.redirect('/posts');
+  });
 }
